2b6e4b98dff85bb9169df08801e9a997fe6ec996
ezepsosa <ezequielperez50@gmail.com>
2024-04-23 15:30:52 +0200

    RRB: Structure and pages changes
    Changed the structure of the project and added About page

--- scrimba - advance react/react-router-blog/src/main.tsx
+++ scrimba - advance react/react-router-blog/src/main.tsx
@@ -1,11 +1,13 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "./App";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App></App>}></Route>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/about" element={<About></About>}></Route>
     </Routes>
   </BrowserRouter>
 );
---
+++ scrimba - advance react/react-router-blog/src/pages/About/About.tsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <h2>Hello, this is the about page!</h2>;
+}
