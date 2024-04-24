f4ac25b948ac09b4fb7c025c1e3528cd9a9442b8
ezepsosa <ezequielperez50@gmail.com>
2024-04-24 21:11:08 +0200

    RRB: Minor changes
    Minor design changes made in Home and About pages

--- scrimba - advance react/react-router-blog/src/pages/About/About.tsx
+++ scrimba - advance react/react-router-blog/src/pages/About/About.tsx
@@ -3,7 +3,7 @@ import "./About.css";
 
 export default function About() {
   return (
-    <div className="about-container">
+    <>
       <div className="banner-container">
         <img
           className="about-container-img"
@@ -11,26 +11,28 @@ export default function About() {
           alt=""
         />
       </div>
-      <h1 className="about-container-text">
-        Don’t squeeze in a sedan when you could relax in a van.
-      </h1>
-      <p className="about-container-text">
-        Our mission is to enliven your road trip with the perfect travel van
-        rental. Our vans are recertified before each trip to ensure your travel
-        plans can go off without a hitch. (Hitch costs extra 😉) <br /> Our team
-        is full of vanlife enthusiasts who know firsthand the magic of touring
-        the world on 4 wheels.
-      </p>
-      <div className="card">
-        <div className="card-text">
-          <h2 className="about-container-text">
-            Your destination is waiting. Your van is ready.
-          </h2>
+      <div className="about-container">
+        <h1 className="about-container-text">
+          Don’t squeeze in a sedan when you could relax in a van.
+        </h1>
+        <p className="about-container-text">
+          Our mission is to enliven your road trip with the perfect travel van
+          rental. Our vans are recertified before each trip to ensure your
+          travel plans can go off without a hitch. (Hitch costs extra 😉) <br />{" "}
+          Our team is full of vanlife enthusiasts who know firsthand the magic
+          of touring the world on 4 wheels.
+        </p>
+        <div className="card">
+          <div className="card-text">
+            <h2 className="about-container-text">
+              Your destination is waiting. Your van is ready.
+            </h2>
+          </div>
+          <Link to="vans" className="btn btn-secondary">
+            Explore our vans
+          </Link>
         </div>
-        <Link to="vans" className="btn btn-secondary">
-          Explore our vans
-        </Link>
       </div>
-    </div>
+    </>
   );
 }
--- scrimba - advance react/react-router-blog/src/pages/Home/Home.tsx
+++ scrimba - advance react/react-router-blog/src/pages/Home/Home.tsx
@@ -7,8 +7,8 @@ function Home() {
         You got the travel plans, we got the travel vans.
       </h1>
       <p className="home-container-text">
-        Add adventure to your life by joining the #vanlife movement. Rent the
-        perfect van to make your perfect road trip.
+        Add adventure to your life by joining the #vanlife movement.
+        <br /> Rent the perfect van to make your perfect road trip.
       </p>
       <Link to="vans" className="btn btn-primary">
         Find your van
