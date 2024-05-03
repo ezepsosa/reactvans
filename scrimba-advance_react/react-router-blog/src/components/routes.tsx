23d24ce54f9542486ae77cfee4156cce09d9dd17
ezepsosa <ezequielperez50@gmail.com>
2024-05-03 12:19:02 +0200

    RRB: Added new pages for Host

--- scrimba-advance_react/react-router-blog/src/components/routes.tsx
+++ scrimba-advance_react/react-router-blog/src/components/routes.tsx
@@ -4,16 +4,23 @@ import About from "../pages/About";
 import VansList from "../pages/Vans/VanList";
 import VansDetail from "../pages/Vans/VanDetails";
 import { Layout } from "./layout";
+import { Dashboard } from "../pages/Host/Dashboard";
+import { Income } from "../pages/Host/Income";
+import { Reviews } from "../pages/Host/Reviews";
 
 export function RoutesComponent() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout></Layout>}>
-          <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/about" element={<About></About>}></Route>
-          <Route path="/vans" element={<VansList></VansList>}></Route>
-          <Route path="/vans/:id" element={<VansDetail></VansDetail>}></Route>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<VansList />} />
+          <Route path="/vans/:id" element={<VansDetail />} />
+          <Route path="/host" element={<Dashboard />}>
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ scrimba-advance_react/react-router-blog/src/pages/Host/Reviews/index.tsx
@@ -0,0 +1,3 @@
+export function Reviews() {
+  return <h1>Hello!</h1>;
+}
